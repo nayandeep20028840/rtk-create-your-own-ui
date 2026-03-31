5991921f22826c1a1953f14a77d1975b57a63315
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2026-03-31 11:45:55 +0530

    sidebar and big screen issue

--- src/components/in-meeting.tsx
+++ src/components/in-meeting.tsx
@@ -1,32 +1,59 @@
 import {
-  RtkStage,
-  RtkGrid,
-  RtkNotifications,
-  RtkParticipantsAudio,
+    RtkStage,
+    RtkGrid,
+    RtkNotifications,
+    RtkParticipantsAudio,
 } from '@cloudflare/realtimekit-react-ui';
 import MeetingHeader from './meeting-header';
 import MeetingControlBar from './meeting-control-bar';
 import MeetingSideBar from './meeting-sidebar';
+import Facetime, { FaceTimeGrid, FaceTimePip } from './facetime';
+
+function InMeetingPreBuilt() {
+    return (
+        <div className="flex flex-col w-full h-full">
+            <header>
+                <MeetingHeader />
+            </header>
+            <main className="flex w-full flex-1">
+                <RtkStage className="flex w-full flex-1 p-2">
+                    <RtkGrid />
+                    <RtkNotifications />
+                    <MeetingSideBar />
+                </RtkStage>
+                <RtkParticipantsAudio />
+            </main>
+            <footer className="flex w-full overflow-visible">
+                <MeetingControlBar />
+            </footer>
+        </div>
+    );
+}
+
+function InMeetingWithFaceTime() {
+    return (
+        <div className="flex flex-col w-full h-full">
+            <header>
+                <MeetingHeader />
+            </header>
+            <main className="flex w-full flex-1">
+                <RtkStage className="flex w-full flex-1 p-2">
+                    <FaceTimeGrid />
+                    <RtkNotifications />
+                    <MeetingSideBar />
+                    <FaceTimePip />
+                </RtkStage>
+                <RtkParticipantsAudio />
+            </main>
+            <footer className="flex w-full overflow-visible">
+                <MeetingControlBar />
+            </footer>
+        </div>
+    );
+}
 
 function InMeeting() {
-  return (
-    <div className="flex flex-col w-full h-full">
-      <header>
-        <MeetingHeader />
-      </header>
-      <main className="flex w-full flex-1">
-        <RtkStage className="flex w-full flex-1 p-2">
-          <RtkGrid />
-          <RtkNotifications />
-          <MeetingSideBar />
-        </RtkStage>
-        <RtkParticipantsAudio />
-      </main>
-      <footer className="flex w-full overflow-visible">
-        <MeetingControlBar />
-      </footer>
-    </div>
-  );
+    return <InMeetingWithFaceTime />;
 }
 
 export default InMeeting;
